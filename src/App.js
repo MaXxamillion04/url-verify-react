40e5680817fddfa05610f687028d52e472c8b91d
Matthew Speller <maxxamillion04@gmail.com>
2020-06-17 01:32:20 -0500

    Spoofed server with URL checking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import APIEndpoint from "./ServerMock";
+
+const urlCheck = require("url");
 
 function App() {
+  let [url, setUrl] = useState("");
+  let [myURL, setMyURL] = useState(urlCheck.parse(""));
+  let [message, setMessage] = useState();
+  let [sentURL, setSentURL] = useState();
+  let [isReady, setIsReady] = useState(true);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setUrl(value);
+
+    sendToAPI(value);
+  };
+
+  function sendToAPI(aURL) {
+    if (checkValidURLFormat(aURL)) {
+      setMessage("Awaiting response from API...");
+
+      if (isReady) {
+        //this boolean is to prevent throttling
+        (async () => {
+          setIsReady(false);
+          setSentURL(aURL);
+          let response = await APIEndpoint(aURL);
+          console.log(response);
+
+          if (response.exists) {
+            setMessage("API Response: " + response.result);
+          } else {
+            setMessage("URL endpoint does not exist. " + response.result);
+          }
+          setIsReady(true);
+        })();
+      } else {
+        setMessage("API is busy, click 'resend' in a moment");
+      }
+    } else {
+      setMessage(
+        "URL is of an invalid format! try  https://cool.jazz/<some extension>"
+      );
+    }
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    sendToAPI(url);
+  };
+
+  function checkValidURLFormat(aURL) {
+    //const URLRegex=/^https:\/\/.+()/;
+    if (aURL === "") {
+      console.log("url is null");
+      return false;
+    }
+    //console.log(typeof aURL);
+    try {
+      /*let preFormat=JSON.stringify(url);
+      preFormat=preFormat.replace(/^\[\"/,"");
+      preFormat=preFormat.replace(/\"\]$/,"");
+      */
+      setMyURL(urlCheck.parse(aURL));
+      //console.log(myURL);
+
+      if (myURL.hostname) {
+        return true;
+      }
+    } catch (error) {
+      //formatted incorrectly
+      console.log("url sucks:" + error);
+      return false;
+    }
+    return false;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <span>Put your URL in here!</span>
+        <form>
+          <label>
+            Try a URL:
+            <input
+              type="text"
+              className="App-input"
+              value={url}
+              onChange={handleChange}
+            />
+            <button className="App-input" onClick={handleClick}>
+              Resend
+            </button>
+          </label>
+        </form>
+        <br />
+        <span>{myURL.href}</span>
+        <br />
+        <br />
+        <span>API Info</span>
+        <span>URL sent: {sentURL}</span>
+        <span>{message}</span>
+        <br/>
+        <br/>
+        <p>scroll down for info on the spoofed server</p>
+      </div>
+
+      <p>
+        the following urls are valid: <br/>https://cool.jazz/hello<br/>
+        https://cool.jazz/about<br/> https://cool.jazz/index<br/> https://cool.jazz/images<br/>
+        images: /popcorn /dog /music <br/>
+        https://cool.jazz/jams <br/>
+        jams: jams/saxBlues, /bigBass, /sweaty, /space
+      </p>
     </div>
   );
 }
---
+++ src/ServerMock.js
@@ -0,0 +1,40 @@
+import React from "react";
+const url = require("url");
+
+async function makeItWait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function APIEndpoint(myUrl) {
+  console.log("request received");
+  let res = "";
+  let exists = false;
+
+    let block = await makeItWait(1200);
+    const URL = url.parse(myUrl);
+
+    let dirStruct = ["/hello", "/about", "/index", "/images", "/jams"];
+    let fileStruct = ["/images/dog","/images/music","/images/popcorn","/jams/saxBlues","/jams/bigBass","/jams/sweaty","/jams/space"];
+    if (URL.hostname === "cool.jazz") {
+        if(!URL.pathname || URL.pathname==='/'){
+            exists=true;
+            res="directory";
+
+        }else
+        if(dirStruct.includes(URL.pathname)){
+            res="directory"
+            exists=true;
+        }else if(fileStruct.includes(URL.pathname)){
+            res="file";
+            exists=true;
+        }
+    } else {
+      res = "That is not our domain. Try: https://cool.jazz";
+      exists = false;
+    }
+  
+
+  return { result: res, exists: exists };
+}
+
+export default APIEndpoint;
